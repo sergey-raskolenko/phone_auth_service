b98a69366b77898e5a85ae4e06a5b88b6672af70
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-12-18 19:48:47 +0300

    Created User models and ObjectManager

--- user/manager.py
+++ user/manager.py
@@ -4,31 +4,29 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
 	use_in_migrations = True
 
-	def _create_user(self, phone=None, password=None, **extra_fields):
-		if not phone:
-			raise ValueError('The given phone must be set')
+	def create_superuser(self, phone, password, **other_fields):
+		other_fields.setdefault('is_staff', True)
+		other_fields.setdefault('is_superuser', True)
+		other_fields.setdefault('is_active', True)
 
-		else:
-			user = self.model(phone=phone, **extra_fields)
-			user.phone = phone
-			user.set_password(password)
-			user.save(using=self._db)
-			return user
+		if other_fields.get('is_staff') is not True:
+			raise ValueError('Superuser must be assigned to is_staff=True')
+		if other_fields.get('is_superuser') is not True:
+			raise ValueError('Superuser must be assigned to is_superuser=True')
 
-	def create_user(self, phone, password=None, **extra_fields):
-		extra_fields.setdefault('is_superuser', False)
-		extra_fields.setdefault('is_staff', False)
-		user = self._create_user(phone=phone, password=password, **extra_fields)
-		user.set_unusable_password()
-		user.save(using=self._db)
+		user = self.create_user(phone, password, **other_fields)
+		user.set_password(password)
+		user.save()
 		return user
 
-	def create_superuser(self, phone, password, **extra_fields):
-		extra_fields.setdefault('is_superuser', True)
-		extra_fields.setdefault('is_staff', True)
-		extra_fields.setdefault('is_active', True)
-
-		if extra_fields.get('is_superuser') is not True:
-			raise ValueError('Superuser must have is_superuser=True.')
-
-		return self._create_user(phone=phone, password=password, **extra_fields)
+	def create_user(self, phone, password, **other_fields):
+		if not phone:
+			raise ValueError('Phone is required!')
+		if password is not None:
+			user = self.model(phone=phone, password=password, **other_fields)
+			user.save()
+		else:
+			user = self.model(phone=phone, password=password, **other_fields)
+			user.set_unusable_password()
+			user.save()
+		return user
--- user/models.py
+++ user/models.py
@@ -1,8 +1,7 @@
 from phonenumber_field.modelfields import PhoneNumberField
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from user.manager import UserManager
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin
 
 NULLABLE = {'null': True, 'blank': True}
 
@@ -10,8 +9,12 @@ NULLABLE = {'null': True, 'blank': True}
 class User(AbstractUser, PermissionsMixin):
 	objects = UserManager()
 
-	phone = PhoneNumberField(max_length=30, verbose_name='phone', unique=True)
 	username = None
+
+	phone = PhoneNumberField(max_length=30, verbose_name='phone', unique=True)
+	otp = models.CharField(max_length=4, verbose_name='otp', **NULLABLE)
+	is_staff = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=False)
 
 	USERNAME_FIELD = "phone"
 	REQUIRED_FIELDS = []
