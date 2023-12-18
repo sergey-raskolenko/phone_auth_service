8ae70305f726228935812fe1aa200ee1d2d40a4e
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-12-18 19:50:04 +0300

    Created OTP work service

---
+++ user/services.py
@@ -0,0 +1,22 @@
+import random
+
+from user.forms import User
+
+
+class OTP:
+
+	@staticmethod
+	def send_otp(phone):
+		otp = str(random.randint(1000, 9999))
+		user = User.objects.get(phone=phone)
+		user.otp = otp
+		user.save()
+		# Some code to send sms
+		print(otp)
+
+	@staticmethod
+	def check_otp(phone, otp):
+		user = User.objects.get(phone=phone)
+		if user.otp == otp:
+			return True
+		return False
