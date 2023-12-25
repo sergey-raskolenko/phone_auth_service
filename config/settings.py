b443b1b5a30ca028fe0c285a5af9f645d4469380
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-12-26 02:24:28 +0300

    Added simple docs with drf-yasg

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 	'rest_framework',
 	"phonenumber_field",
+	'drf_yasg',
 
 	'user',
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from user.api.views import RegisterAPIView, LoginAPIView, LogoutAPIView, OTPAPIView, ProfileAPIView
 from user.views import register_page, check_otp, login_page, generate_otp, profile_page, enter_invite_code, logout_view
 
+
+schema_view = get_schema_view(
+	openapi.Info(
+		title="PhoneAuthService Documentation",
+		default_version='v0.1',
+		description="Basic description of the project's API structure",
+		terms_of_service="https://www.example.com/policies/terms/",
+		contact=openapi.Contact(email="sergey.raskolenko@gmail.com"),
+		license=openapi.License(name="BSD License"),
+	),
+	public=True,
+	permission_classes=(permissions.AllowAny,),
+	)
+
 urlpatterns = [
+	path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+	path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+urlpatterns += [
 	path('admin/', admin.site.urls),
 	path('register/', register_page, name="register"),
 	path('check/', check_otp, name="check_otp"),
